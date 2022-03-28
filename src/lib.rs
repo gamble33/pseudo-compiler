a392b9699376218cffa137079956127b5d8d803b
Slopey <giafilmsyt@gmail.com>
2022-03-28 16:25:16 +0200

    Add Documentation for Parser

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+//! Front-End Compiler for Cambdridge's psuedo-code.
+
 pub mod lexer;
+pub mod parser;
 
 pub type Result<T> = std::result::Result<T, String>;
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,19 @@
 use pseudo_compiler::lexer::Lexer;
+use pseudo_compiler::parser::Parser;
+
+use std::process;
 
 fn main() {
+
+    // Lexical Analysis
     let mut lexer: Lexer = Lexer::from_file("test.pseudo").unwrap();
-    while let Some(t) = lexer.next() {
-        println!("{:?}", t);
-    };
+    let tokens = lexer
+        .map(|token| token.unwrap_or_else(|x| {
+            println!("Lexing Error: {:?}", x);
+            process::exit(1);
+        }))
+        .collect::<Vec<_>>();
+    
+    // Syntactic Analysis
+    let parser: Parser = Parser::new(tokens.into_iter().peekable());
 }
